62f8dc3b0adad2afeb6dec6c7f5c15b80569029f
biswasuser <lohanibiswas1@gmail.com>
2025-12-21 20:24:05 +0545

    completed SideBar Section

--- frontend/src/components/SIdeBar.jsx
+++ frontend/src/components/SIdeBar.jsx
@@ -3,14 +3,15 @@ import { useAppContext } from '../context/AppContext'
 import { assets } from '../assets/assets'
 import moment from 'moment'
 
-const SIdeBar = () => {
+const SIdeBar = ({isMenuOpen, setIsMenuOpen}) => {
 
     const {chats, setSelectedChats, theme, setTheme, user, navigate} = useAppContext()
 
     const [search, setSearch] = useState('')
+    
 
   return (
-    <div className='flex flex-col h-screen min-w-72 p-5 dark:bg-linear-to-b from-[#242124]/30 to-[#000000]/30 border border-gray-300 backdrop:blur-3xl transition-all duration-500 max-md:absolute left-0 z-1'>
+    <div className={`flex flex-col h-screen min-w-72 p-5 dark:bg-linear-to-b from-[#242124]/30 to-[#000000]/30 border border-gray-300 backdrop:blur-3xl transition-all duration-500 max-md:absolute left-0 z-1 ${!isMenuOpen && 'max-md:-translate-x-full'}`}>
     {/* Logo */}
       <img src={theme === 'dark' ? assets.logo_full: assets.logo_full_dark} alt=""  className='w-full max-w-48'/>
 
@@ -35,7 +36,7 @@ const SIdeBar = () => {
             chats.filter((chat ) => 
                 chat.messages[0] ? chat.messages[0] ?. content.toLowerCase().includes(search.toLowerCase()) : chat.name.toLowerCase().includes(search.toLowerCase())).map((chat) => {
 
-                    return <div key={chat._id} className='p-2 px-4 dark:bg-[#57317C]/10 border border-gray-300 dark:border-[#80609F]/15 rounded-md cursor-pointer flex justify-between group'>
+                    return <div onClick={() => {navigate('/'); setSelectedChats(chat); setIsMenuOpen(false)}} key={chat._id} className='p-2 px-4 dark:bg-[#57317C]/10 border border-gray-300 dark:border-[#80609F]/15 rounded-md cursor-pointer flex justify-between group'>
 
                         <div>
                             <p className='truncate w-full'>{chat.messages.length > 0 ? chat.messages[0].content.slice(0,32) : chat.name}</p>
@@ -50,7 +51,7 @@ const SIdeBar = () => {
     </div>
 
     {/* community images */}
-    <div onClick={() => {navigate('/community')}} className='flex items-center gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer hover:scale-103 transition-all duration-200'>
+    <div onClick={() => {navigate('/community'); setIsMenuOpen(false)}} className='flex items-center gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer hover:scale-103 transition-all duration-200'>
         <img src={assets.gallery_icon} alt="" className='w-4.5 not-dark:invert' />
         <div className='flex flex-col text-sm'>
             <p>Community Images</p>
@@ -59,7 +60,7 @@ const SIdeBar = () => {
 
 
     {/* Credit Purchase Option */}
-    <div onClick={() => {navigate('/credits')}} className='flex items-center gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer hover:scale-103 transition-all duration-200'>
+    <div onClick={() => {navigate('/credits'); setIsMenuOpen(false)}} className='flex items-center gap-2 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md cursor-pointer hover:scale-103 transition-all duration-200'>
         <img src={assets.diamond_icon} alt="" className='w-4.5 dark:invert' />
         <div className='flex flex-col text-sm'>
             <p>Credits : {user?.credits}</p>
@@ -81,6 +82,15 @@ const SIdeBar = () => {
             <span className='absolute left-1 top-1 w-3 h-3 bg-white rounded-full transition-transform peer-checked:translate-x-4'></span>
         </label>
     </div>
+
+    {/* User Account */}
+     <div  className='flex items-center gap-3 p-3 mt-4 border border-gray-300 dark:border-white/15 rounded-md group'>
+        <img src={assets.user_icon} alt="" className='w-7 not-dark:invert' />
+        <p className='flex-1 text-sm dark:text-primary truncate'>{user? user.name : 'Login your account'}</p>
+        {user && <img src={assets.logout_icon} className='h-5 cursor-pointer hidden not-dark:invert group-hover:block'/>}
+    </div>
+
+    <img onClick={() => setIsMenuOpen(false)} src={assets.close_icon} alt="" className='absolute top-3 right-3 w-5 h-5 cursor-pointer md:hidden not-dark:invert' />
 
     </div>
   )
